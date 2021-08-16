cd011146ff54d9300f79029c61689650e90e838c
Lakyn <lakynmphotography@hotmail.com>
2021-08-16 13:31:17 -0400

    changes

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function StartUp(option) {
 
 
 function viewAllEmployees() {
-  connection.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function (error, res) {
+  db.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function (error, res) {
     console.table(res);
     closeMenu\();
   })
@@ -121,14 +121,14 @@ function viewAllEmployees() {
 
 function viewAllDepartments() {
   console.log("view all departments")
-  connection.query("SELECT * from departments", function (error, res) {
+  db.query("SELECT * from departments", function (error, res) {
     console.table(res);
     closeMenu();
   })
 }
 
 function viewAllRoles() {
-  connection.query("SELECT * from roles", function (error, res) {
+  db.query("SELECT * from roles", function (error, res) {
     console.table(res);
     closeMenu();
   })
