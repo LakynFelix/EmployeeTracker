30770f568aea6458c41d1432a6f0dab32d3de192
Lakyn <lakynmphotography@hotmail.com>
2021-08-16 14:02:34 -0400

    changes

--- index.js
+++ index.js
@@ -18,22 +18,23 @@ const db = mysql.createConnection(
 var roles;
 var departments;
 var employees;
+var data;
 
 
 
-db.query("SELECT * FROM roles", function (error, res) {
-  role = res.map(roles => ({ name: roles.title, value: roles.id }))
-})
-db.query("SELECT * FROM departments", function (error, res) {
-  departments = res.map(departments => ({ name: title 
-    , value: department_id }))
-})
-db.query("SELECT * FROM employee", function (error, res) {
+// db.query("SELECT * FROM roles", function (error, res) {
+//   role = res.map(roles => ({ name: roles.title, value: roles.id }))
+// })
+// db.query("SELECT * FROM departments", function (error, res) {
+//   departments = res.map(departments => ({ name: title 
+//     , value: department_id }))
+// })
+// db.query("SELECT * FROM employee", function (error, res) {
  
-  employees = res.map(employees => ({ name: `${employee.first_name} ${employee.last_name}`, value: employee.id }))
-})
+//   employees = res.map(employees => ({ name: `${employee.first_name} ${employee.last_name}`, value: employee.id }))
+// })
 
-StartUp();
+startUp();
 
 
 
@@ -115,7 +116,7 @@ function StartUp(option) {
 function viewAllEmployees() {
   db.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function (error, res) {
     console.table(res);
-    closeMenu\();
+    closeMenu();
   })
 }
 
@@ -164,16 +165,16 @@ function addEmployee() {
   })
 }
 
-db.query("INSERT INTO employee ",
-{
-  first_name: data.firstName,
-  last_name: data.lastName,
-  role_id: data.title,
-  manager_id: data.manager
-}, function (error, res) {
-  if (error) throw error;
-})
-closeMenu();
+// db.query("INSERT INTO employee ",
+// {
+//   first_name: data.firstName,
+//   last_name: data.lastName,
+//   role_id: data.title,
+//   manager_id: data.manager
+// }, function (error, res) {
+//   if (error) throw error;
+// })
+// closeMenu();
 
 
 function addDepartment() {
@@ -211,15 +212,15 @@ function addRole() {
     addRole(response)
   })
 }
-db.query("INSERT INTO role ",
-{
-  role_id: data.title,
-  salary: data.salary,
+// db.query("INSERT INTO role ",
+// {
+//   role_id: data.title,
+//   salary: data.salary,
 
-}, function (error, res) {
-  if (error) throw error;
-})
-closeMenu();
+// }, function (error, res) {
+//   if (error) throw error;
+// })
+// closeMenu();
 
 
 function updateRole() {
