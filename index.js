fa9dc2fab294e99bb4d902f5c254ad890b571d24
Lakyn <lakynmphotography@hotmail.com>
2021-08-16 13:13:54 -0400

    changes

--- index.js
+++ index.js
@@ -15,30 +15,33 @@ const db = mysql.createConnection(
   console.log(`Connected to the employeedb database.`)
 );
 
-const role;
-const departments;
-const employees;
+var roles;
+var departments;
+var employees;
 
-// simple query
+
+
 db.query("SELECT * FROM roles", function (error, res) {
-  role = res.map(roles => ({ name: roles.title, value: role.id }))
+  role = res.map(roles => ({ name: roles.title, value: roles.id }))
 })
 db.query("SELECT * FROM departments", function (error, res) {
-  departments = res.map(dep => ({ name: dep.name, value: dep.id }))
+  departments = res.map(departments => ({ name: title 
+    , value: department_id }))
 })
-connection.query("SELECT * FROM employee", function (error, res) {
+db.query("SELECT * FROM employee", function (error, res) {
  
-  employees = res.map(emp => ({ name: `${emp.first_name} ${emp.last_name}`, value: emp.id }))
+  employees = res.map(employees => ({ name: `${employee.first_name} ${employee.last_name}`, value: employee.id }))
 })
 
 StartUp();
 
 
-Function StartUp() {
+
+function startUp() {
   inquirer.prompt(
     {
       type: "list",
-      message: "Welcome to My EmployeeTracker. What would you like to do first?",
+      message: "Welcome to My EmployeeTracker. What would you like to do?",
       name: "choices",
       choices: [
         {
@@ -76,9 +79,9 @@ Function StartUp() {
       ]
     }).then(function (res){
      
-    StartUp(res.choices)
+    startUp(res.choices)
     })
-}
+};
 
 function StartUp(option) {
   switch (option) {
@@ -108,6 +111,30 @@ function StartUp(option) {
   }
 }
 
+
+function viewAllEmployees() {
+  connection.query("SELECT employee.id, employee.first_name, employee.last_name, roles.title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;", function (error, res) {
+    console.table(res);
+    endOrMenu();
+  })
+}
+
+function viewAllDepartments() {
+  console.log("view all departments")
+  connection.query("SELECT * from department", function (error, res) {
+    console.table(res);
+    endOrMenu();
+  })
+}
+
+function viewAllRoles() {
+  connection.query("SELECT * from role", function (error, res) {
+    console.table(res);
+    endOrMenu();
+  })
+}
+
+
 function addEmployee() {
   inquirer.prompt([
     {
@@ -136,6 +163,17 @@ function addEmployee() {
     addEmployee(response)
   })
 }
+
+db.query("INSERT INTO employee ",
+{
+  first_name: data.firstName,
+  last_name: data.lastName,
+  role_id: data.title,
+  manager_id: data.manager
+}, function (error, res) {
+  if (error) throw error;
+})
+closeMenu();
 
 
 function addDepartment() {
@@ -173,6 +211,16 @@ function addRole() {
     addRole(response)
   })
 }
+db.query("INSERT INTO role ",
+{
+  role_id: data.title,
+  salary: data.salary,
+
+}, function (error, res) {
+  if (error) throw error;
+})
+closeMenu();
+
 
 function updateRole() {
   inquirer.prompt([
@@ -193,9 +241,8 @@ function updateRole() {
 }
 
 function updateEmployeeRole(data) {
-  connection.query(`UPDATE Employee role_id = ${data.titleID} WHERE id = ${data.empID}`,
+  db.query(`UPDATE Employee role_id = ${data.titleID} WHERE id = ${data.empoyeeID}`,
   function (error, res) {
-    // console.log(error, res);
     if (error) throw error;
   });
   closeMenu();
@@ -210,8 +257,5 @@ function closeMenu() {
   });
 }
 
-function end() {
-  console.log("Thank you Goodbye :) ");
-  connection.end();
-  process.exit();
-}
+
+
